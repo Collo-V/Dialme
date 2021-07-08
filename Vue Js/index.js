5c4c2c8d33d3b503d7eb5493f2d9393b087d73c7
Colo-V <collinsvictoromondi@gmail.com>
2021-07-08 08:45:50 +0300

    Vue js changes

--- Vue Js/index.js
+++ Vue Js/index.js
@@ -7,6 +7,13 @@ new Vue({
         email:"https://mailto:collinsvictoromondi@gmail.com",
         divt:'<div class="divs">We are back online. Grab your favourite items on our <a href="http://www.fineteklabs.com">Website</a>',
         dwnlink:"",
+        comida:"",
+        timer:10,
+        start:'<button v-on:click="startgame" ref="start">Start</button>',
+        words:0,
+        avg:0,
+        speed:10,highest:0
+
     },
     methods:{
         Show:function(){
@@ -29,7 +36,48 @@ new Vue({
         greet="Buenas noches"
     }
     return greet+" "+this.name+". Como estas?"
+},
+update: function(event){
+    // console.log(event);
+    this.comida=this.$refs.likes.value.toUpperCase();
+        console.log(this.$refs);
         },
+        startgame: function (){
+            txt=this.$refs.likes;
+            txt.onpaste = e => {
+                e.preventDefault();
+                return false}
+            
+           this.timer-=1;
+           if(this.timer<=0){
+               txt.disabled=true;
+               document.getElementById("startbtn").style.display="none";
+               document.getElementById("gameover").style.display="block";
+               a=this.comida.length;
+               word=this.words=this.comida.split(" ");
+            //    word = word.every(myFunction);
+
+            // function myFunction(value) {
+            // if(value!=""){return value};}
+            
+               this.words=word.length;
+               this.avg=parseInt(a/this.words);
+               this.speed=this.words*6;if(this.speed>this.highest){this.highest=this.speed}
+               console.log(this.comida)
+               return
+            }
+           
+           setTimeout(this.startgame,1000)
+                       
+
+        },
+        restart:function(){
+            this.$refs.likes.value="",
+            this.timer=10;
+            this.$refs.likes.disabled=false;
+            document.getElementById("startbtn").style.display="block";
+            document.getElementById("gameover").style.display="none";
+            }
     
     }
 
