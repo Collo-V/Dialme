8fb15010b5ea1e8508124299f0e391b65b293b19
Collo-V <60650741+Collo-V@users.noreply.github.com>
2021-10-01 23:01:35 +0300

    Update settings.py

--- dialme/settings.py
+++ dialme/settings.py
@@ -152,3 +152,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 sys.path.append("..") # Adds higher directory to python modules path.
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
