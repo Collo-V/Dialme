ba7c0c0f781a8ea40fe7cee4a2ecdfb671d7ec45
Collo-V <60650741+Collo-V@users.noreply.github.com>
2021-10-01 23:05:29 +0300

    Update settings.py

--- dialme/settings.py
+++ dialme/settings.py
@@ -157,3 +157,5 @@ sys.path.append("..") # Adds higher directory to python modules path.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
